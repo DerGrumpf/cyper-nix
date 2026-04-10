757f8076c20d5ed87da9e548983d3abbed140a4c
DerGrumpf <phil.keier@hotmail.com>
2026-04-10 20:09:19 +0200

    Added Cyper Node 1 configuration

---
+++ nixos/server/default.nix
@@ -0,0 +1,14 @@
+{ ... }:
+{
+  services.nginx = {
+    enable = true;
+    virtualHosts.localhost = {
+      locations."/" = {
+        return = "200 '<html><body>It works</body></html>'";
+        extraConfig = ''
+          default_type text/html;
+        '';
+      };
+    };
+  };
+}
