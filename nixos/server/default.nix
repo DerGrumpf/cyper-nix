3a0c00fb65b3d7a2357b95b296db70da98c6a4a1
DerGrumpf <phil.keier@hotmail.com>
2026-04-10 21:53:03 +0200

    Nginx check works

--- nixos/server/default.nix
+++ nixos/server/default.nix
@@ -9,6 +9,14 @@
           default_type text/html;
         '';
       };
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 80;
+        }
+      ];
     };
   };
+
+  networking.firewall.allowedTCPPorts = [ 80 ];
 }
