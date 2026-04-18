496a2eba21adfd0c888674d5e89cf40e1a17998a
DerGrumpf <phil.keier@hotmail.com>
2026-04-18 09:50:12 +0200

    Bit of a clean up

--- nixos/roles/unifi.nix
+++ nixos/roles/unifi.nix
@@ -9,18 +9,4 @@
     mongodbPackage = pkgs.mongodb-7_0;
     openFirewall = true; # opens 3478/udp, 10001/udp, 8080, 8443, 8843, 8880, 6789
   };
-
-  networking.firewall = {
-    allowedTCPPorts = [
-      8443
-      8080
-      8880
-      8843
-      6789
-    ];
-    allowedUDPPorts = [
-      3478
-      10001
-    ];
-  };
 }
