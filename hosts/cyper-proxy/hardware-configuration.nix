b944652549732e2697f454a83bfd8400e584954b
DerGrumpf <phil.keier@hotmail.com>
2026-04-24 23:29:27 +0200

    Changed Proxy stuff

--- hosts/cyper-proxy/hardware-configuration.nix
+++ hosts/cyper-proxy/hardware-configuration.nix
@@ -1,21 +1,35 @@
 { lib, modulesPath, ... }:
-
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
   boot = {
-    initrd.availableKernelModules = [ ];
+    initrd.availableKernelModules = [
+      "virtio_pci"
+      "virtio_blk"
+      "virtio_net"
+      "ahci"
+      "xhci_pci"
+    ];
     initrd.kernelModules = [ ];
     kernelModules = [ ];
     extraModulePackages = [ ];
-    loader.grub.enable = false;
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
   };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXROOT";
-    fsType = "ext4";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXROOT";
+      fsType = "ext4";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-label/NIXBOOT";
+      fsType = "vfat";
+    };
   };
 
   swapDevices = [ ];
