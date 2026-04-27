fa0cb9cc5988c05fd50b96e2bb0bec6f12f38685
DerGrumpf <phil.keier@hotmail.com>
2026-04-27 09:55:54 +0200

    Added Jitsi

--- hosts/cyper-proxy/configuration.nix
+++ hosts/cyper-proxy/configuration.nix
@@ -3,22 +3,32 @@
   imports = [
     ./hardware-configuration.nix
     ../../nixos/roles/nginx.nix
+    ../../nixos/roles/jitsi.nix
   ];
 
   networking = {
-  hostName = "cyper-proxy";
-  useDHCP = false;
-  interfaces.ens3 = {
-    ipv4.addresses = [
-      { address = "178.254.8.35"; prefixLength = 23; }
+    hostName = "cyper-proxy";
+    useDHCP = false;
+    interfaces.ens3 = {
+      ipv4.addresses = [
+        {
+          address = "178.254.8.35";
+          prefixLength = 23;
+        }
+      ];
+    };
+    defaultGateway = "178.254.8.1";
+    nameservers = [
+      "178.254.16.151"
+      "178.254.16.141"
     ];
+
+    firewall.allowedTCPPorts = [
+      80
+      443
+    ];
+
   };
-  defaultGateway = "178.254.8.1";
-  nameservers = [ "178.254.16.151" "178.254.16.141" ];
-
-  firewall.allowedTCPPorts = [ 80 443 ];
-
-};
 
   system.stateVersion = "26.05";
 }
