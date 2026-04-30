35598f777b5ce9f7d369911bac91dc0045e3a32d
DerGrumpf <phil.keier@hotmail.com>
2026-04-30 13:45:34 +0200

    WIP: livekit

--- nixos/roles/matrix/lk-jwt.nix
+++ nixos/roles/matrix/lk-jwt.nix
@@ -1,19 +1,24 @@
-{ config, ... }:
+{ config, lib, ... }:
 let
   domain = "cyperpunk.de";
   synapseUrl = "http://100.109.179.25:8008";
 in
 {
-  sops.secrets.livekit_key = { };
+  sops.secrets.livekit_key_jwt = { };
+
+  networking.firewall.allowedTCPPorts = [ 18080 ];
 
   services.lk-jwt-service = {
     enable = true;
-    keyFile = config.sops.secrets.livekit_key.path;
+    keyFile = config.sops.secrets.livekit_key_jwt.path;
     livekitUrl = "wss://cyperpunk.de/livekit/sfu";
   };
 
-  systemd.services.lk-jwt-service.environment = {
-    LIVEKIT_FULL_ACCESS_HOMESERVERS = domain;
-    MATRIX_BASE_URL = synapseUrl;
+  systemd.services.lk-jwt-service = {
+    environment = {
+      LIVEKIT_FULL_ACCESS_HOMESERVERS = domain;
+      MATRIX_BASE_URL = synapseUrl;
+      LIVEKIT_JWT_BIND = lib.mkForce ":18080";
+    };
   };
 }
