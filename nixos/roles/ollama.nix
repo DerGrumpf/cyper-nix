ba8ee8c3516d3fdc957a2287d6e0cc175765ffcf
DerGrumpf <phil.keier@hotmail.com>
2026-06-17 18:41:54 +0200

    Configured Ollama

---
+++ nixos/roles/ollama.nix
@@ -0,0 +1,18 @@
+_: {
+  services.ollama = {
+    enable = true;
+
+    host = "0.0.0.0";
+    port = 11434;
+
+    openFirewall = true;
+
+    loadModels = [
+      "llama3.2:3b"
+      "qwen2.5:3b"
+      "deepseek-r1:1.5b"
+    ];
+
+    syncModels = true;
+  };
+}
